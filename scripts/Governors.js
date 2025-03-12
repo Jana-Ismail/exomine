2755b8cbd1f17bc7de5fc99764b49c65c6cc8ee0
Jana Ismail <jana.ismail.nss@gmail.com>
2025-03-12 15:49:24 -0500

    Defined Governors component function | called function in the main html text in the render() function in main.js

--- scripts/Governors.js
+++ scripts/Governors.js
@@ -4,5 +4,24 @@ import { getGovernors } from "../managers/governorManager.js";
 // Component function to render the html for a select dropdown with options for the active governors
 // inside the #governor-select-container in the main.js render() function
 export const Governors = async () => {
-    
+    // Fetch the governors
+    const governors = await getGovernors()
+
+    let html = `
+        <label for="governor-select-dropdown">Choose Governor</label>
+        <select id="governor-select-dropdown">
+            <option value="0"></option>
+    `
+
+    html += governors.map(governor => {
+        if (governor.isActive) {
+            return `
+                <option value="${governor.id}">${governor.name}</option>
+            `
+        }
+    })
+
+    html += `</select>`
+
+    return html
 }
