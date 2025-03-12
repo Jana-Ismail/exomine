e17a4fc4740dea61d557f374d73d70cdba8b464d
Jana Ismail <jana.ismail.nss@gmail.com>
2025-03-12 01:34:56 -0500

    Updated the resource in the fetch url to 'facilityMinerals' rather than 'facilities'

--- managers/facilityMineralManager.js
+++ managers/facilityMineralManager.js
@@ -1,6 +1,6 @@
 // Getter function to fetch facilityMinerals from database.json file
 export const getFacilityMineralsFromFacilityId = async (facilityId) => {
-    const facilityMineralsFetch = await fetch(`http://localhost:8088/facilities?facilityId=${facilityId}&_expand=mineral`)
+    const facilityMineralsFetch = await fetch(`http://localhost:8088/facilityMinerals?facilityId=${facilityId}&_expand=mineral`)
     const facilityMineralsArr = await facilityMineralsFetch.json()
 
     return facilityMineralsArr
