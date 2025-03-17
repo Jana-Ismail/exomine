578bbbccbe9e246d039c23168e7350d03f8447ee
Jana Ismail <jana.ismail.nss@gmail.com>
2025-03-17 17:44:48 -0500

    Defined logic for Cart() component function to return html for a 'Space Cart' header and a 'Purchase Mineral' button

--- scripts/Cart.js
+++ scripts/Cart.js
@@ -2,6 +2,12 @@
 import { purchaseMineral } from "./TransientState.js"
 
 // Component function to render html for a "Purchase Mineral" button
-export const Cart = () => {
+export const Cart = async () => {
+    return `
+        <h2 class="cart-header">Space Cart</h2>
+        <div class="selected-facility-mineral-container>
 
+        </div>
+        <button type="submit" class="purchase-mineral-btn">Purchase Mineral</button>
+    `
 }
